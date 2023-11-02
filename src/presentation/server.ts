cf4a708a46d163934991db0124b7935876e912d2
Alejandro Dubon <alejandrodubon88@gmail.com>
2023-11-01 23:06:12 -0600

    feat: add cron service adapter

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,11 @@
+import { CronService } from "./cron/cron-service";
+
 export class Server {
   public static start(): void {
-    console.log('Server started...')
+    console.log('Server started...');
+    CronService.createJob('*/5 * * * * *', () => {
+      const date = new Date();
+      console.log(`Cron job executed at ${date.toLocaleString()}`);
+    });
   }
 }
